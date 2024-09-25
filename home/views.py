6abd53a33d90fd650979265b67f0f8bf55171868
Alysson-Silva <alyssonlogar123@gmail.com>
2024-09-24 21:49:52 -0300

    Configurando templates globais com DIRS + extends para herança de templates

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'index.html') 
+    return render(request, 'home/index.html') 
 
 def trabalhe_conosco(request):
     return HttpResponse("TRABALHE CONOSCO HOMEVIEWS")
--- sobre/views.py
+++ sobre/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def sobre(request):
-    return render(request, 'sobre.html')
+    return render(request, 'sobre/sobre.html')
 
 def trabalhe_conosco(request):
-    return render(request, 'trabalhe_conosco.html')
+    return render(request, 'sobre/trabalhe_conosco.html')
